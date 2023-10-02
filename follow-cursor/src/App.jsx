253a4bf58f04c43f6995f00145b1f177f9901ab5
reisel <reiselvalle@gmail.com>
2023-10-02 17:09:51 -0400

    end

--- follow-cursor/src/App.jsx
+++ follow-cursor/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ enabled, setEnabled ] = useState(false);
+  const [ position, setPosition ] = useState({ x: 0, y: 0 });
+
+  useEffect(() =>{
+    const handleEvent = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x:clientX, y: clientY });
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleEvent)
+    }
+    
+    return ()=>{
+       window.removeEventListener('pointermove', handleEvent);
+    }
+  }, [enabled])
+
+  const cirlceClassName = `${enabled? "circle" : "hide"}`;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <div className= { cirlceClassName } style={
+        {
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }
+      }></div>
+      <h1> Follow the cursor </h1>
+      <button onClick={ ()=> setEnabled(!enabled) }>
+        { enabled ? 'Disable' : 'Enable' }
+      </button>
+    </main>
   )
 }
 
